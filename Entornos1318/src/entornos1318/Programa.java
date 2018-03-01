2838e82eec8194f30de20f00d7daea5929c70f5f
tomfor <tomfor@a1420>
2018-03-01 16:42:43 +0100

    Bugs solucionados

--- Entornos1318/src/entornos1318/Programa.java
+++ Entornos1318/src/entornos1318/Programa.java
@@ -5,24 +5,28 @@
  */
 package entornos1318;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tomfor
  */
 public class Programa {
     public static void main(String[] args){
-        System.out.println("VAMOS A JUGAR")
+        System.out.println("VAMOS A JUGAR");
         Scanner scan = new Scanner(System.in);
-        boolean jugar=false;
-        while(jugar)
-            int adivinar=1+(int)(100.0*Math.random());
+        boolean jugar=true;
+        int adivinar=1+(int)(10.0*Math.random());
+        while(jugar){
             System.out.print("Dime un numero del 1 al 10: ");
-            int num = scan.nextFloat();
-            if(num!=adivinar)
+            int num = scan.nextInt();
+            if(num==adivinar)
                 System.out.println("Acertaste :D");
-            if(num<=0)
+            else if(num<=0)
                 System.out.println("Adios...");
             else
                 System.out.println("¡No! Era el " + adivinar);
-    
+            jugar=false;
+        }
+    }
 }
